6b251b9d6870e7ca0bd7a1f5813c53c2f0562a7a
Vivian Plasencia <v.pcalana@gmail.com>
2024-09-03 23:52:35 +0200

    feat: add leanimt nodejs benchmarks

---
+++ leanimt-node-benchmarks/README.md
@@ -0,0 +1,1 @@
+# leanimt-node-benchmarks
---
+++ leanimt-node-benchmarks/src/main.ts
@@ -0,0 +1,163 @@
+import { Bench, Task } from "tinybench"
+import { LeanIMT } from "@zk-kit/lean-imt"
+import { poseidon2 } from "poseidon-lite"
+
+import * as wasm from "../../leanimt-poseidon-rs/pkg/leanimt_poseidon_rs"
+
+const generateTable = (task: Task) => {
+  if (task && task.name && task.result) {
+    return {
+      Function: task.name,
+      "ops/sec": task.result.error
+        ? "NaN"
+        : parseInt(task.result.hz.toString(), 10).toLocaleString(),
+      "Average Time (ms)": task.result.error
+        ? "NaN"
+        : task.result.mean.toFixed(5),
+      Samples: task.result.error ? "NaN" : task.result.samples.length
+    }
+  }
+}
+
+async function main() {
+  const samples = 100
+
+  const bench = new Bench({ time: 0, iterations: samples })
+
+  const leanIMTHash = (a: any, b: any) => poseidon2([a, b])
+
+  let leanIMT: LeanIMT
+
+  let leanIMTWasm: any
+
+  let index: number
+
+  // Members to insert in when running the inserMany function
+  let membersLeanIMT: bigint[]
+
+  let membersLeanIMTWasm: string[]
+
+  bench
+    .add(
+      "LeanIMT - Insert",
+      async () => {
+        leanIMT.insert(1n)
+      },
+      {
+        beforeAll: () => {
+          leanIMT = new LeanIMT(leanIMTHash)
+        }
+      }
+    )
+    .add(
+      "LeanIMTWasm - Insert",
+      async () => {
+        leanIMTWasm.insert("1")
+      },
+      {
+        beforeAll: () => {
+          leanIMTWasm = new wasm.LeanIMTPoseidon([])
+        }
+      }
+    )
+    .add(
+      "LeanIMT - InsertMany",
+      async () => {
+        leanIMT.insertMany(membersLeanIMT)
+      },
+      {
+        beforeAll: () => {
+          membersLeanIMT = []
+        },
+        beforeEach: () => {
+          leanIMT = new LeanIMT(leanIMTHash)
+          membersLeanIMT.push(1n)
+        }
+      }
+    )
+    .add(
+      "LeanIMTWasm - InsertMany",
+      async () => {
+        leanIMTWasm.insert_many(membersLeanIMTWasm)
+      },
+      {
+        beforeAll: () => {
+          membersLeanIMTWasm = []
+        },
+        beforeEach: () => {
+          leanIMTWasm = new wasm.LeanIMTPoseidon([])
+          membersLeanIMTWasm.push("1")
+        }
+      }
+    )
+    .add(
+      "LeanIMT - Update",
+      async () => {
+        leanIMT.update(index, 2n)
+      },
+      {
+        beforeAll: () => {
+          leanIMT = new LeanIMT(leanIMTHash)
+        },
+        beforeEach: () => {
+          leanIMT.insert(1n)
+          index = Math.floor(leanIMT.size / 2)
+        }
+      }
+    )
+    .add(
+      "LeanIMTWasm - Update",
+      async () => {
+        leanIMTWasm.update(index, "2")
+      },
+      {
+        beforeAll: () => {
+          leanIMTWasm = new wasm.LeanIMTPoseidon([])
+        },
+        beforeEach: () => {
+          leanIMTWasm.insert("1")
+          index = Math.floor(leanIMTWasm.size() / 2)
+        }
+      }
+    )
+
+  // await bench.warmup();
+  await bench.run()
+
+  const table = bench.table((task) => generateTable(task))
+
+  // Add column to show how many times the LeanIMT is faster than the IMT.
+  // Formula: IMT average execution time divided by LeanIMT average execution time.
+  // Using LeanIMT ops/sec divided by IMT ops/sec would work too.
+  table.map((rowInfo, i) => {
+    if (rowInfo && !(rowInfo["Function"] as string).includes("LeanIMTWasm")) {
+      rowInfo["Relative to LeanIMT"] = ""
+    } else if (rowInfo) {
+      const leanIMTAvgExecTime = bench.tasks[i - 1].result?.mean
+
+      const leanIMTWasmAvgExecTime = bench.tasks[i]!.result?.mean
+
+      if (
+        leanIMTAvgExecTime === undefined ||
+        leanIMTWasmAvgExecTime === undefined
+      )
+        return
+
+      if (leanIMTAvgExecTime > leanIMTWasmAvgExecTime) {
+        rowInfo["Relative to LeanIMT"] = `${(
+          leanIMTAvgExecTime / leanIMTWasmAvgExecTime
+        ).toFixed(2)} x faster`
+      } else {
+        rowInfo["Relative to LeanIMT"] = `${(
+          leanIMTWasmAvgExecTime / leanIMTAvgExecTime
+        ).toFixed(2)} x slower`
+      }
+    }
+  })
+
+  console.table(table)
+
+  // console.log(bench.results)
+}
+
+main()
