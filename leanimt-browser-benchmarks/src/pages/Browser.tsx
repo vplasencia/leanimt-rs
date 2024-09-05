8a9e61fada4fb526a227614d9cbbc442fb1b7aea
Vivian Plasencia <v.pcalana@gmail.com>
2024-09-05 15:13:45 +0200

    refactor: improve browser page

--- leanimt-browser-benchmarks/src/pages/Browser.tsx
+++ leanimt-browser-benchmarks/src/pages/Browser.tsx
@@ -7,13 +7,6 @@ import Table from "../components/Table"
 export default function Browser() {
   const [tableInfo, setTableInfo] = useState()
 
-  // useEffect(()=> {
-  //   const wasmFunction = async () => {
-  //     await generateBenchmarks()
-  //   }
-  //   wasmFunction()
-  // }, [])
-
   const renderTable = async () => {
     const benchmarksInfo = await generateBenchmarks()
     setTableInfo(benchmarksInfo as any)
@@ -21,15 +14,27 @@ export default function Browser() {
 
   return (
     <div>
-      <button onClick={renderTable}>Generate Benchmarks</button>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      {tableInfo ? (
-        <div className="flex justify-center items-center my-5">
-          <Table data={tableInfo} />
+      <div className="flex flex-col justify-center items-center my-10">
+        <div className="w-96 text-xl text-center">
+          Generate the browser benchmarks for the LeanIMT in TypeScript and the
+          LeanIMT WebAssembly (Wasm) compiled from the Rust implementation.
         </div>
-      ) : (
-        <></>
-      )}
+        <button
+          onClick={renderTable}
+          className="mt-5 rounded-md bg-blue-700 py-2 px-5 font-semibold hover:bg-blue-600 transition-colors duration-150 text-white"
+        >
+          Generate Benchmarks
+        </button>
+      </div>
+      <div>
+        {tableInfo ? (
+          <div className="flex justify-center items-center my-5">
+            <Table data={tableInfo} />
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
     </div>
   )
 }
