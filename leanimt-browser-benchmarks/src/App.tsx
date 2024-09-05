d6fd7713b41baeae72d1ef04dd9ff31b884ca493
Vivian Plasencia <v.pcalana@gmail.com>
2024-09-05 23:26:29 +0200

    refactor: remove unused code

--- leanimt-browser-benchmarks/src/App.tsx
+++ leanimt-browser-benchmarks/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react"
+import { useState } from "react"
 
 import TableNode from "./components/TableNode"
 
@@ -8,49 +8,7 @@ import { Link } from "react-router-dom"
 
 import InsertLineChart from "./components/InsertLine"
 
-export default function Browser() {
-  // const [tableInfo, setTableInfo] = useState(FunctionsBrowser)
-  const [functionsBrowser, setFunctionsBrowser] = useState({
-    options: {
-      chart: {
-        id: "id"
-      },
-      xaxis: {
-        categories: [0]
-      }
-    },
-    series: [
-      {
-        name: "series",
-        data: [0]
-      }
-    ]
-  })
-
-  // useEffect(() => {
-  //   const functionsBrowserData = {
-  //     options: {
-  //       chart: {
-  //         id: "basic-bar"
-  //       },
-  //       xaxis: {
-  //         categories: [1]
-  //       }
-  //     },
-  //     series: [
-  //       {
-  //         name: "series-1",
-  //         data: [1]
-  //       }
-  //     ]
-  //   }
-  //   setFunctionsBrowser(temp)
-  // }, [])
-
-  if (!functionsBrowser) {
-    return <div>Loading...</div>
-  }
-
+export default function App() {
   return (
     <div>
       <Link to="/browser">Browser Benchmarks</Link>
