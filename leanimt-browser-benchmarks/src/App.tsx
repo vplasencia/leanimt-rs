5d24813f131504b089a152730bcbcf00ad6bf135
Vivian Plasencia <v.pcalana@gmail.com>
2024-09-06 20:06:00 +0200

    docs: update main page content

--- leanimt-browser-benchmarks/src/App.tsx
+++ leanimt-browser-benchmarks/src/App.tsx
@@ -10,6 +10,31 @@ export default function App() {
     <div className="flex flex-col min-h-screen px-2 text-slate-950">
       <Header />
       <div className="mb-auto">
+        <div className="flex justify-center items-center space-x-1">
+          The following benchmarks were conducted on a computer with the system
+          and software specifications listed
+          <a
+            className="flex space-x-1 text-indigo-500 hover:underline ml-1"
+            href="https://github.com/vplasencia/leanimt-rs?tab=readme-ov-file#benchmarks"
+            target="_blank"
+            rel="noreferrer noopener nofollow"
+          >
+            <span>here</span>
+          </a>
+          .
+        </div>
+        <div className="flex justify-center items-center space-x-1">
+          To know more about this project, visit the project
+          <a
+            className="flex space-x-1 text-indigo-500 hover:underline ml-1"
+            href="https://github.com/vplasencia/leanimt-rs?tab=readme-ov-file#js-x-rust-x-wasm"
+            target="_blank"
+            rel="noreferrer noopener nofollow"
+          >
+            <span>Readme file</span>
+          </a>
+          .
+        </div>
         <div className="flex flex-col justify-center items-center my-10">
           <div className="text-3xl font-medium">Node.js</div>
           <TableNode />
